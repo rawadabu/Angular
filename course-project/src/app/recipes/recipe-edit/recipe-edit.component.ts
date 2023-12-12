5fb37feb36f199404dd2b1d3ededf05e879b9f9b
rawadabu <rawadabusaleh0@gmail.com>
2023-12-12 12:21:52 +0200

    Adding validating user input, submitting the recipe edit form, adding a delete and clear functionality, redirecting user after deleteing a recipe, adding an image preview

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
-import { Recipe } from '../recipe.model';
+// import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -16,7 +16,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -62,17 +63,23 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    const newRecipe = new Recipe(
-      this.recipeForm.value('name'),
-      this.recipeForm.value('description'),
-      this.recipeForm.value('imagePath'),
-      this.recipeForm.value['ingredients']
-    );
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value('name'),
+    //   this.recipeForm.value('description'),
+    //   this.recipeForm.value('imagePath'),
+    //   this.recipeForm.value['ingredients']
+    // );
     if (this.editMode) {
-      this.recipeService.updateRecipe(this.id, newRecipe);
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
     } else {
-      this.recipeService.addRecipe(newRecipe);
+      this.recipeService.addRecipe(this.recipeForm.value);
     }
+
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   getingredientsArray() {
--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -3,9 +3,12 @@ import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { recipesMockData } from './recipes.mock-data';
+import { Subject } from 'rxjs';
 
 @Injectable() // INJECT SERVICE TO A SERVICE (WE NEED TO ACCESS THE SHOPPING LIST SERVICE)
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>(); // CAN EMIT OUR INGREDIENT ARRAY
+
   constructor(private slService: ShoppingListService) {}
 
   recipeSelected = new EventEmitter<Recipe>();
@@ -26,9 +29,16 @@ export class RecipeService {
 
   addRecipe(recipe: Recipe) {
     this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   updateRecipe(index: number, newRecipes: Recipe) {
     this.recipes[index] = newRecipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
